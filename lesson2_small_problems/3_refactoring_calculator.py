e746b6ff370c6e9f8a39d78f0e63b983f813f085
Ethan Noble <ethan.noble23@gmail.com>
2024-09-04 22:38:39 -0400

    add internationalization to json and .py files

--- lesson2_small_problems/3_refactoring_calculator.py
+++ lesson2_small_problems/3_refactoring_calculator.py
@@ -6,10 +6,20 @@ with open('calculator_messages.json', 'r') as file:
 def prompt(message):
     print(f"=>{message}")
 
-prompt(MESSAGES["welcome"])
+prompt(MESSAGES["lang_select"])
+language = input()
+
+while language not in ['EN', 'ES', 'FR', 'DE']:
+    prompt(MESSAGES["lang_select"])
+    language = input()
+
+intl_message = MESSAGES[language]
+
+
+prompt(intl_message["welcome"])
 
 while True:
-    prompt(MESSAGES["asks"]["numbers"][0])
+    prompt(intl_message["ask"]["number"][0])
     number1 = input()
 
     def invalid_number(number_str):
@@ -22,24 +32,24 @@ while True:
 
 
     while invalid_number(number1):
-        prompt(MESSAGES["scolds"]["wrong_number"])
+        prompt(intl_message["scold"]["wrong_number"])
         number1 = input()
 
-    prompt(MESSAGES["asks"]["numbers"][1])
+    prompt(intl_message["ask"]["number"][1])
     number2 = input()
 
 
     while invalid_number(number2):
-        prompt(MESSAGES["scolds"]["wrong_number"])
+        prompt(intl_message["scold"]["wrong_number"])
         number2 = input()
 
     prompt(f'{number1} {number2}')
-    prompt(MESSAGES["asks"]["operations"])
+    prompt(intl_message["ask"]["operation"])
     operation = input()
 
 
     while operation not in ['1', '2', '3', '4']:
-        prompt(MESSAGES["scolds"]["wrong_operation"])
+        prompt(intl_message["scold"]["wrong_operation"])
         operation = input()
 
 
@@ -55,20 +65,19 @@ while True:
             output = int(number1) / int(number2)
 
     # print the result to the terminal.
-    prompt(f'The result is: {output}')
-    
+    prompt(intl_message["result"])
+    prompt(output)    
 
-    prompt(MESSAGES["asks"]["repeat"])
+    prompt(intl_message["ask"]["repeat"])
     continue_choice = input()
 
-    while continue_choice not in ['Y', 'N']:
-        prompt(MESSAGES["scolds"]["wrong_continue"])
+    while continue_choice not in [intl_message["letter"][0], intl_message["letter"][1]]:
+        prompt(intl_message["scold"]["wrong_continue"])
         continue_choice = input()
     
-    match continue_choice:
-        case 'Y':
-            prompt(MESSAGES["confirmations"]["yes"])
+    if continue_choice == intl_message["letter"][0]:
+            prompt(intl_message["confirmation"]["yes"])
             continue
-        case 'N':
-            prompt(MESSAGES["confirmations"]["no"])
+    if continue_choice == intl_message["letter"][1]:
+            prompt(intl_message["confirmation"]["no"])
             break
