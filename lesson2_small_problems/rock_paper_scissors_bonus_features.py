7b053f27f51802cabbe96c02d3c47848b8092f78
Ethan Noble <ethan.noble23@gmail.com>
2024-09-27 00:50:10 -0400

    ui tweaks & documentation

--- lesson2_small_problems/rock_paper_scissors_bonus_features.py
+++ lesson2_small_problems/rock_paper_scissors_bonus_features.py
@@ -1,8 +1,30 @@
+'''
+This version of Rock, Paper, Scissors includes two additional choices:
+Lizard and Spock.
+
+ROCK crushes SCISSORS, LIZARD
+PAPER covers ROCK, disproves SPOCK
+SCISSORS cut PAPER, LIZARD
+LIZARD chews on PAPER, poisons SPOCK
+SPOCK vaporizes ROCK, smashes SCISSORS
+
+User is prompted to enter a choice. Computer then randomly selects 
+from one of the five options, and a winner for the round is determined.
+
+User and computer play to best three out of five. 
+
+Ties count toward the turn total and a tie final result is possible. 
+'''
+
 import random
 
 def prompt(message):
+    '''
+    Distinguish computer messages from user input in terminal.
+    '''
     print(f'==> {message}')
 
+# Valid user inputs
 VALID_CHOICES = {
     'r': 'rock',
     'p': 'paper',
@@ -10,6 +32,16 @@ VALID_CHOICES = {
     'l': 'lizard',
     'sp': 'spock'
 }
+
+# Game logic
+WINNING_COMBINATIONS = {
+    'rock':     ['scissors',    'lizard'],
+    'paper':    ['rock',        'spock'],
+    'scissors': ['paper',       'lizard'],
+    'lizard':   ['paper',       'spock'],
+    'spock':    ['rock',        'scissors']
+}
+
 
 def get_player_choice():
     prompt('Choose one: (r)ock, (p)aper, (sc)issors, (l)izard, (sp)ock')
@@ -19,120 +51,75 @@ def get_player_choice():
         prompt("That's not a valid choice.")
         choice = input()
 
-    # Return corresponding value for input's key
+    # Return the value that corresponds to user input's key
     return VALID_CHOICES[choice]
 
 def is_choice_valid(choice):
     if choice in VALID_CHOICES:
         return True
-
     return False
 
 def get_computer_choice():
     computer_choice = random.choice(list(VALID_CHOICES.values()))
     return computer_choice
 
-def display_choices(player, computer):
-    prompt(f"You chose {player}, computer chose {computer}")
+def display_choices(player_choice, computer_choice):
+    prompt(f"You chose {player_choice}. The computer chose {computer_choice}.")
 
-def determine_outcome(player, computer):
-    '''
-    Based on player and computer inputs, return round outcome as
-    win, loss, or tie for player
-    '''
-    if ((player == "rock" and computer == "scissors") or
-        (player == "rock" and computer == "lizard") or
-        (player == "paper" and computer == "rock") or
-        (player == "paper" and computer == "spock") or
-        (player == "scissors" and computer == "paper") or
-        (player == "scissors" and computer == "lizard") or
-        (player == "lizard" and computer == "spock") or
-        (player == "spock" and computer == "rock") or
-        (player == "lizard" and computer == "paper") or
-        (player == "spock" and computer == "scissors")):
-        return 'win'
+def player_wins_round(player_choice, computer_choice):
+    return computer_choice in WINNING_COMBINATIONS[player_choice]
 
-    if ((player == "rock" and computer == "paper") or
-        (player == "rock" and computer == "spock") or
-        (player == "paper" and computer == "scissors") or
-        (player == "paper" and computer == "lizard") or
-        (player == "scissors" and computer == "rock") or
-        (player == "scissors" and computer == "spock") or
-        (player == "lizard" and computer == "scissors") or
-        (player == "lizard" and computer == "rock") or
-        (player == "spock" and computer == "lizard") or
-        (player == "spock" and computer == "paper")):
-        return 'loss'
+def display_round_outcome(player_choice, computer_choice):
+    if player_wins_round(player_choice, computer_choice):
+        prompt("You win this round!")
+    elif player_choice == computer_choice:
+        prompt("This round's a tie!")
+    else:
+        prompt("Computer wins this round!")
 
-    return 'tie'
-
-def display_outcome(outcome):
-    match outcome:
-        case 'win':
-            prompt("You win this round!")
-        case 'loss':
-            prompt("Computer wins this round!")
-        case 'tie':
-            prompt("This round's a tie!")
-
-
-
-def keep_score(outcome, player_score, computer_score):
+def adjust_score(player_choice, computer_choice, player_score, computer_score):
     '''
     Increment the score variables
     '''
-    match outcome:
-        case 'win':
-            player_score += 1
-
-        case 'loss':
-            computer_score += 1
-        case 'tie':
-            pass
+    if player_wins_round(player_choice, computer_choice):
+        player_score += 1
+    elif player_choice == computer_choice:
+        pass
+    else:
+        computer_score += 1
     return player_score, computer_score
 
+def display_score(player_score, computer_score):
+    prompt(f"SCORE: You: {player_score} | Computer: {computer_score}")
 
-def display_score(player, computer):
-    prompt(f"You: {player} | Computer: {computer}")
+def display_turns(turns):
+    prompt(f"ROUND: {turns}/5")
 
+def player_wins_game(player_score):
+    return player_score >= 3
 
-def player_wins(score):
+def computer_wins_game(computer_score):
+    return computer_score >= 3
+
+def display_winner(player_score, computer_score):
     '''
-    Check if player has won
+    Display game's final outcome.
     '''
-    if score == 3:
-        return True
-
-    return False
-
-def computer_wins(score):
-    '''
-    Check if computer has won
-    '''
-    if score == 3:
-        return True
-
-    return False
-
-
-def display_winner(player, computer):
-    '''
-    Display game's final outcome
-    '''
-    if computer_wins(computer):
+    if computer_wins_game(computer_score) or computer_score > player_score:
         prompt("Computer won! Better luck next time.")
-    if player_wins(player):
+    elif player_wins_game(player_score) or player_score > computer_score:
         prompt("You won! Way to go!")
-
+    else:
+        prompt("Tie! Try again.")
 
 def get_play_again_choice():
     '''
-    After final score, ask if player wants to go again
+    After final score, ask if player wants to go again.
     '''
     prompt("Do you want to play again? (y/n)")
     answer = input().lower()
 
-    while not answer or answer[0] not in ['y', 'n']:
+    while not answer or answer not in ['y', 'n']:
         prompt("That's not a valid choice.")
         answer = input().lower()
 
@@ -146,27 +133,40 @@ def get_play_again_choice():
 def main():
     continue_game = True
 
-
     while continue_game is True:
+        turns = 1
         player_score = 0
         computer_score = 0
+        prompt("%====================================================%")
+        prompt("|  WELCOME TO ROCK, PAPER, SCISSORS, LIZARD, SPOCK!  |")
+        prompt("%====================================================%")
 
-        # While neither the player nor the computer have 3 points:
-        while(not player_wins(player_score)
-              and not computer_wins(computer_score)):
+        prompt('''
+                           RULES:
+               ROCK crushes SCISSORS, LIZARD
+               PAPER covers ROCK, disproves SPOCK
+               SCISSORS cut PAPER, LIZARD
+               LIZARD chews on PAPER, poisons SPOCK
+               SPOCK vaporizes ROCK, smashes SCISSORS
+               ''')
+
+        while(not player_wins_game(player_score)
+              and not computer_wins_game(computer_score)
+              and turns <= 5):
 
             player_choice = get_player_choice()
             computer_choice = get_computer_choice()
             display_choices(player_choice, computer_choice)
+            display_round_outcome(player_choice, computer_choice)
 
-            round_outcome = determine_outcome(player_choice, computer_choice)
-            display_outcome(round_outcome)
-
-            player_score, computer_score = keep_score(round_outcome,
-                                                      player_score,
-                                                      computer_score)
+            player_score, computer_score = adjust_score(player_choice,
+                                                        computer_choice,
+                                                        player_score,
+                                                        computer_score)
 
             display_score(player_score, computer_score)
+            display_turns(turns)
+            turns += 1
 
         display_winner(player_score, computer_score)
         continue_game = get_play_again_choice()
